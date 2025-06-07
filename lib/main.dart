aeded89557833921cb3600314800ba954d2b7bd0
tav1o <159470143+tav1o@users.noreply.github.com>
2025-06-07 18:59:32 -0500

    Revert "Creacion vista login"

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'vistas/cargando.dart';
 import 'vistas/carta.dart';
 import 'vistas/falla.dart';
@@ -10,10 +11,11 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: inicio(),
     );
   }
