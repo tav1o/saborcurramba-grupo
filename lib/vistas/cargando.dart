b3e84d68a1deea204fa5502e3545ec6f3cdb278b
mariacamacho10 <mariaangelcamacho4@gmail.com>
2025-06-08 16:39:56 -0500

    Creación de vista loading
    Esta vista de loading muestra una pantalla centrada con un indicador de carga (CircularProgressIndicator) y el texto "cargando..." con una fuente de tamaño 15. Todo está alineado al centro usando un column.

--- lib/vistas/cargando.dart
+++ lib/vistas/cargando.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 
-class cargando extends StatelessWidget {
-  const cargando({
-    super.key,
-  });
+class loading extends StatelessWidget {
+  const loading({super.key}); // constructor de widget
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Center(
-        
+
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center, 
+          children: [
+            CircularProgressIndicator(), 
+            SizedBox(height: 20),
+            Text(
+              'Cargando...',
+              style: TextStyle(fontSize: 15),
+            )
+          ],
+        ),
       ) ,
     );
   }
