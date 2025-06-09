ff2c65db7ce53de7323fc55cf77a9f7a8e83e0a0
Juan <Juandavidrhenalsmontero@gmail.com>
2025-06-09 10:45:42 -0500

    creacion del bloc-state
    Creacion del estado aqui representa en qué situación se encuentra tu aplicación o componente en un momento dado .

--- lib/bloc/home_state.dart
+++ lib/bloc/home_state.dart
@@ -8,3 +8,6 @@ sealed class HomeState extends Equatable {
 }
 
 final class HomeInitial extends HomeState {}
+final class HomeLoadInprogress extends HomeState{}
+final class HomeLoadSucess extends HomeState{}
+final class HomeLoadFailure extends HomeState{}
